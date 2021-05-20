a750da7f75228c97631485009a231e8aacce283e
Milan Vashisth <milanvashisth1122@gmail.com>
2021-05-20 18:02:22 +0530

    Debounce

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback, useRef, useEffect } from "react";
+import debounce from "lodash.debounce";
+
+import "./App.css";
+
+function useDebounce(callback, delay) {
+  const debouncedFn = useCallback(
+    debounce((...args) => callback(...args), delay),
+    [delay] // will recreate if delay changes
+  );
+  return debouncedFn;
+}
+
+function useDebounceAlt(callback, delay) {
+  const memoizedCallback = useCallback(callback, []);
+  const debouncedFn = useRef(debounce(memoizedCallback, delay));
+
+  useEffect(() => {
+    debouncedFn.current = debounce(memoizedCallback, delay);
+  }, [memoizedCallback, debouncedFn, delay]);
+
+  return debouncedFn.current;
+}
 
 function App() {
+  const [value, setValue] = useState("");
+  const [dbValue, saveToDb] = useState(""); // would be an API call normally
+
+  const debouncedSave = useDebounce((nextValue) => saveToDb(nextValue), 1000);
+
+  const handleChange = (event) => {
+    const { value: nextValue } = event.target;
+    setValue(nextValue);
+    debouncedSave(nextValue);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>Blog</h1>
+      <textarea value={value} onChange={handleChange} rows={5} cols={50} />
+      <section className="panels">
+        <div>
+          <h2>Editor (Client)</h2>
+          {value}
+        </div>
+        <div>
+          <h2>Saved (DB)</h2>
+          {dbValue}
+        </div>
+      </section>
+    </main>
   );
 }
 
